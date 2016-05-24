928e0251a92967cf88054ffd2d2502dba4c939ee
wei.xu <wei.xu@10xgenomics.com>
2016-05-24 16:26:04 -0700

    undo the changes done by the automatic formatting

--- bgzf/reader.go
+++ bgzf/reader.go
@@ -10,9 +10,9 @@ import (
 	"code.google.com/p/biogo.bam/bgzf/flate"
 	"code.google.com/p/biogo.bam/bgzf/gzip"
 
-	"errors"
 	"io"
 	"io/ioutil"
+	"errors"
 )
 
 type Reader struct {
@@ -140,7 +140,7 @@ func (bg *Reader) Read(p []byte) (int, error) {
 			} else {
 				bg.offset.File += int64(bs)
 				if bg.offset.File != bg.cr.n {
-					bg.err = errors.New("bgzf: use of closed writer")
+					 bg.err =  errors.New("bgzf: use of closed writer");
 					break
 				}
 			}
