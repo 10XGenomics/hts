3c2d1747f2bf09e73a5cf74dac76b6b00088b4ef
wei.xu <wei.xu@10xgenomics.com>
2017-06-01 13:14:54 -0700

    this is a little bit odd. but now we check the reads to make sure they are mapped out of the fetch function

--- reader.go
+++ reader.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"io"
 	"unsafe"
+	"fmt"
 )
 
 type Reader struct {
@@ -207,7 +208,7 @@ func (iter FetchIter) Next() bool {
     }
 
 	_, err := iter.br.Read(iter.rec)
-	if err != nil {
+	if err != nil || iter.rec.Ref == nil {
 		//fmt.Println("find a nil")
 		return false
 	}
